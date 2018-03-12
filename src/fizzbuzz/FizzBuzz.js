d2f1f21a3e33bc54b33ed844a7a212c709a315ea
Bill <wcharlesm@gmail.com>
2018-03-12 13:35:56 -0400

    initialize project with stubbed out function and a few failing unit tests

---
+++ src/fizzbuzz/FizzBuzz.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './FizzBuzz.css'
+
+class FizzBuzz extends Component {
+
+  generateList(size) {
+    var foo = [];
+    return foo;
+  }
+
+  render() {
+    const count = 100;
+    const fizzBuzzList = this.generateList(count);
+    const fizzyList = fizzBuzzList.map((element) => <li>{element}</li>)
+    return (
+      <ol className="FizzBuzz">
+        {fizzyList}
+      </ol>
+    );
+  }
+}
+
+export default FizzBuzz;
---
+++ src/fizzbuzz/FizzBuzz.test.js
@@ -0,0 +1,23 @@
+import FizzBuzz from './FizzBuzz';
+
+test ('it displays numbers', () => {
+  const fizzy = new FizzBuzz();
+  const results = fizzy.generateList(2);
+
+  expect(results[0]).toBe(1);
+  expect(results[1]).toBe(2);
+})
+
+test ('it can fizz', () => {
+    const fizzy = new FizzBuzz();
+    const results = fizzy.generateList(3);
+  
+    expect(results[2]).toBe('fizz');
+})
+
+test ('it can buzz', () => {
+    const fizzy = new FizzBuzz();
+    const results = fizzy.generateList(5);
+  
+    expect(results[4]).toBe('buzz');
+})
